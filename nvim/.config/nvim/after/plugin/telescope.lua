9914a927290bd164a93bddac5ffdfff8c3d30f12
Vinicius Zani <806442+viniciuszani@users.noreply.github.com>
2023-03-06 12:50:18 -0300

    Configure telescope file browser with <C-f>

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,21 @@
 local builtin = require('telescope.builtin')
 
+require("telescope").setup {
+  extensions = {
+    file_browser = {
+      theme = "ivy",
+      -- disables netrw and use telescope-file-browser in its place
+      hijack_netrw = true,
+    },
+  },
+}
+
+-- To get telescope-file-browser loaded and working with telescope,
+-- you need to call load_extension, somewhere after setup function:
+require("telescope").load_extension "file_browser"
+
+local file_browser= require("telescope").extensions.file_browser
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
@@ -15,15 +31,12 @@ end)
 vim.keymap.set('n', '<C-s>', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<C-t>', builtin.lsp_references, {})
 
-require("telescope").setup {
-  extensions = {
-    file_browser = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-    },
-  },
-}
--- To get telescope-file-browser loaded and working with telescope,
--- you need to call load_extension, somewhere after setup function:
-require("telescope").load_extension "file_browser"
+-- Telescope file browsing.
+vim.keymap.set('n', '<C-f>', function() file_browser.file_browser() end, {})
+--vim.api.nvim_set_keymap(
+--  "n",
+--  "<C-f>",
+--  ":Telescope file_browser",
+--  { noremap = true }
+--)
+
