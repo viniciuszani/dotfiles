e1ff0e1be7ca5a4fdb501bc071d0e3ed6884601f
Vinicius Zani <806442+viniciuszani@users.noreply.github.com>
2023-03-15 09:52:51 -0300

    Remaps fix / removal.

--- nvim/.config/nvim/lua/zani/remap.lua
+++ nvim/.config/nvim/lua/zani/remap.lua
@@ -92,15 +92,8 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 -- Disable Q.
 vim.keymap.set("n", "Q", "<nop>")
 
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
 -- Find and replace using the current token as a template.
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Easier to navigate on tabs.
 vim.keymap.set("n", "<C-l>", "<cmd>tabnext<CR>")
